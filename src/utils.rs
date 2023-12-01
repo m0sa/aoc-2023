1af367f8f48a87a8fe936dd5e070df63dc36415a
m0sa <samo.prelog@gmail.com>
2023-12-01 23:07:59 +0000

    tweaking utils so they work in a vscode debugging session

--- src/utils.rs
+++ src/utils.rs
@@ -3,8 +3,19 @@ use std::fs;
 use std::path::Path;
 
 pub fn resource(project_relative_path: &str) -> String {
-    let cargo_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
-    let final_path = Path::new(&cargo_dir).join(&project_relative_path);
+    let current_dir = match env::current_dir() {
+        Result::Ok(s) => match s.to_str() {
+            Some(val) => Result::Ok(val.to_string()),
+            None => Result::Err(env::VarError::NotPresent),
+        },
+        Result::Err(_) => Result::Err(env::VarError::NotPresent),
+    };
+
+    let cargo_dir = env::var("CARGO_MANIFEST_DIR");
+    let root_dir = cargo_dir
+        .or(current_dir)
+        .expect("cannot resolve root resource directory");
+    let final_path = Path::new(&root_dir).join(&project_relative_path);
     return fs::read_to_string(final_path)
         .expect(&format!("Could not read '{project_relative_path}'"));
 }
